d71969bb7f8cc919fe8563491005153fa75a7eb7
danovd <danovski_dimitar@abv.bg>
2023-07-11 11:34:48 +0300

    generating combinations

---
+++ _01_Recursion_LAB/_07_Generating_Combinations.java
@@ -0,0 +1,12 @@
+package _01_Recursion_LAB;
+
+public class _07_Generating_Combinations {
+    public static void main(String[] args) {
+
+
+        //  char c = '8';
+      //  int i = Character.getNumericValue(c);
+       // System.out.println(i);
+
+    }
+}
