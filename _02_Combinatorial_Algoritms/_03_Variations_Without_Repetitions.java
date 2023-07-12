b48ca2460cc0dbe6580de2d6fdcff6d17abd6a16
danovd <danovski_dimitar@abv.bg>
2023-07-12 07:26:48 +0300

    variations without repetitions

---
+++ _02_Combinatorial_Algoritms/_03_Variations_Without_Repetitions.java
@@ -0,0 +1,48 @@
+package _02_Combinatorial_Algoritms;
+
+
+import java.util.Scanner;
+
+public class _03_Variations_Without_Repetitions {
+    public static String[] elements;
+    public static String[] variations;
+    public static boolean[] used;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        elements = sc.nextLine().split("\\s+");
+
+        int k = Integer.parseInt(sc.nextLine());
+        variations = new String[k];
+        used = new boolean[elements.length];
+
+        variations(0);
+
+    }
+
+    public static void variations(int index){
+        if(index == variations.length){
+            print(variations);
+            return;
+        }
+        for (int i = 0; i < elements.length; i++) {
+            if(!used[i]){
+                used[i] = true;
+                variations[index] = elements[i];
+                variations(index + 1);
+                used[i] = false;
+            }
+        }
+    }
+
+    private static void swap(String [] arr, int first, int second) {
+        String temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static void print(String [] arr) {
+        System.out.println(String.join(" ", arr));
+    }
+}
